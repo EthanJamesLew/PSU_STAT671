7498b28c41ec13a8b8104a2909f9b3a687337ddf
Ethan Lew <elew@pdx.edu>
2019-10-10 22:09:31 -0700

    Added kernalized version

--- hw/simple_classifier/classifier.py
+++ hw/simple_classifier/classifier.py
@@ -5,6 +5,7 @@ Ethan Lew
 A simple classifier designed to match the description of STAT 671 lecture 1.
 '''
 import numpy as np
+from numba import jit
 
 class Classifier:
     def __init__(self, N):
@@ -17,7 +18,15 @@ class Classifier:
         self.cp = np.zeros((N))
         self.X = None
         self.Y = None
+        self.k = np.dot
 
+    @property
+    def mn(self):
+        return np.sum(self.Y == -1)
+
+    @property
+    def mp(self):
+        return np.sum(self.Y == 1)
     @property
     def c(self):
         return (self.cp + self.cn)/2
@@ -53,20 +62,60 @@ class Classifier:
         self.cn = np.mean(self.X[idxn], axis=0)
         self.cp = np.mean(self.X[idxp], axis=0)
 
-    def classify(self, x):
+    def classify_kernel(self, x):
         ''' classify
         given an observation x, predict the label
         :param x: (N) sample of dimensionality N
         :return: y label in {-1, 1}
         '''
+
+        g = 0.0
+
+        Xn = self.X[self.Y == -1, :]
+        Xp = self.X[self.Y == 1, :]
+
+        for xi, yi in zip(self.X, self.Y):
+            if yi == 1:
+                g += 1 / self.mp * np.dot(x, xi)
+            else:
+                g -= 1 / self.mn * np.dot(x, xi)
+
+        for xi in Xp:
+            for xj in Xp:
+                g -= 1 / (2 * self.mp ** 2) * np.dot(xi, xj)
+
+        for xi in Xn:
+            for xj in Xn:
+                g += 1 / (2 * self.mn ** 2) * np.dot(xi, xj)
+
+        return np.sign(g)
+
+    def classify(self, x):
         return int(np.sign(np.dot(self.cp - self.cn, x - self.c)))
 
+
+
+@jit(nopython=True)
+def k_polynomial(x, xp, d):
+    return np.dot(x, xp)**d
+
+
+@jit(nopython=True)
+def k_gaussian(x, xp, sigma):
+    return np.exp(-np.linalg.norm(x-xp)**2/(2*sigma**2))
+
+
+@jit(nopython=True)
+def k_sigmoid(x, xp, kappa, Theta):
+    return np.tanh(kappa * np.dot(x, xp) + Theta)
 
 if __name__ == '__main__':
     from sklearn.datasets import load_iris
     import pandas as pd
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
+
+    from sklearn.decomposition import PCA
 
     iris_sk = load_iris()
     species_lp = {0: 'I.setosa', 1: 'I. versicolor', 2: 'I. virginica'}
@@ -87,16 +136,17 @@ if __name__ == '__main__':
 
 
     # Get the two species to classify
-    test1_df = iris_df.loc[iris_df['species'] != 2]
+    test1_df = iris_df.loc[iris_df['species'] != 0]
 
     # Get training and validation sets
     train1_df, ver1_df = partition_df(test1_df, 0.8)
 
     iris_classifier = Classifier(4)
+    #iris_classifier.k = lambda x,xp: np.dot(x, xp)
 
     iris_classifier.train(np.array(train1_df)[:, :-1], np.array(train1_df)[:, -1] * 2 - 1)
 
-    val  = np.array([iris_classifier.classify(x) for x in np.array(ver1_df)[:, :-1]])
+    val  = np.array([iris_classifier.classify_kernel(x) for x in np.array(ver1_df)[:, :-1]])
     print(val)
     print(np.array(ver1_df, dtype=np.int)[:, -1]*2-1 )
 
@@ -104,19 +154,23 @@ if __name__ == '__main__':
     y = np.array(test1_df)[:, -1]
     U, S, Vh = np.linalg.svd(D, full_matrices=True)
 
+    pca = PCA(n_components=3)
+    X_reduced = pca.fit_transform(D)
+
     U = U[:3, :4]
     Dr = U@D.T
     Dr = Dr.T
     fig = plt.figure(1, figsize=(8, 6))
     ax = Axes3D(fig, elev=-150, azim=110)
-    ax.scatter(Dr[:, 0], Dr[:, 1], Dr[:, 2], c=y,
+    ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=y,
                cmap=plt.cm.Set1, edgecolor='k', s=10)
     cp = iris_classifier.cp
-    cp = U@cp
+    cn = iris_classifier.cn
+    cp = pca.transform(cp.reshape(1, -1))[0]
+    cn = pca.transform(cn.reshape(1, -1))[0]
+    print(cp)
     ax.scatter(cp[0], cp[1], cp[2],
                cmap=plt.cm.Set1, edgecolor='k', s=100)
-    cn = iris_classifier.cn
-    cn = U @ cn
     ax.scatter(cn[0], cn[1], cn[2],
                cmap=plt.cm.Set1, edgecolor='k', s=100)
     ax.set_title("First three PCA directions")
