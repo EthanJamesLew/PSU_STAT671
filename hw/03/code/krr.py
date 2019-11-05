0be03bbaca7537088ebdbf7399b1f3ceb5f1bdf8
Ethan Lew <elew@pdx.edu>
2019-11-05 09:21:42 -0800

    Refactored hw code

--- hw/03/code/krr.py
+++ hw/03/code/krr.py
@@ -10,8 +10,24 @@ import numpy as np
 from inspect import signature
 from label_data import LabelData, PartitionData
 
+##KERNELS
+def k_polynomial(x, xp, d):
+    return (np.dot(x, xp)+1)**d
+
+
+def k_gaussian(x, xp, sigma):
+    return np.exp(-np.sum((x-xp)**2)/(2*(sigma**2)))
+
+
+def k_tanh(x, xp, kappa, Theta):
+    return np.tanh(kappa * np.dot(x, xp) + Theta)
 
 def kernel_mat(f, x):
+    '''
+    :param f: kernel function
+    :param x: vector of values
+    :return: K symmetric matrix
+    '''
     n = len(x)
     K = np.zeros((n, n))
     for i in range(0, n):
@@ -22,6 +38,12 @@ def kernel_mat(f, x):
     return K
 
 def lstsq(A, b):
+    '''
+    Modified least squares solver (Ax = b )
+    :param A:
+    :param b:
+    :return: x
+    '''
     AA = A.T @ A
     bA = b @ A
     D, U = np.linalg.eigh(AA)
@@ -30,14 +52,27 @@ def lstsq(A, b):
     return np.linalg.lstsq(Ap, bp, rcond=None)
 
 def alpha(data, k, l):
+    '''
+    Solve for the alphas (KRR problem)
+    :param data:
+    :param k: kernel function
+    :param l: regularization factor
+    :return:
+    '''
     n = data.n
     K = kernel_mat(k, data.x)
     G = K + l*n*np.eye(n)
     return np.linalg.solve(G, data.y)
-    #return np.linalg.pinv(G) @ data.y
 
 
 def krr(data, alpha, k, x):
+    '''
+    :param data:
+    :param alpha:
+    :param k:
+    :param x:
+    :return:
+    '''
     total  = 0
     for idx, xd in enumerate(data.x):
         total += alpha[idx]*k(xd, x)
@@ -77,18 +112,9 @@ class KernelRidgeRegression:
         self._alpha = alpha(self._data, self._k, self._l)
 
     def __call__(self, x):
-        if type(x) is float:
-            return krr(self._data, self._alpha, self._k, x)
-        else:
-            m = x.shape[0]
-            ans = np.zeros((m))
-            for idx, xi in enumerate(x):
-                ans[idx] = krr(self._data, self._alpha, self._k, xi)
-            return ans
-
+        return np.array([krr(self._data, self._alpha, self._k, xi) for xi in x])
 
 if __name__ == '__main__':
-
     print(kernel_mat(np.dot, [1,2,3]))
     data = np.array([[2], [1], [7], [9]])
     labs = np.array([0.4, 1.2, 3.4, -0.4])
